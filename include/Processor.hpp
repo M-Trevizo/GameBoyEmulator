31df7e86c6b825277ffd6de61e9ed15bb513b55c
Michael Trevizo <michael.trevizo93@yahoo.com>
2023-11-20 20:47:47 -0800

    Add fetch and decode functions.

--- include/Processor.hpp
+++ include/Processor.hpp
@@ -2,18 +2,26 @@
 #define PROCESSOR_HPP
 
 #include <cstdint>
+#include "./Cartridge.hpp"
 
-const uint16_t MEM_SIZE = 65536;
+const uint16_t MEM_SIZE = 65535;
+const uint16_t VRAM_START = 0x8000;
 
 class Processor {
 
     public:
 
-        uint16_t memory[MEM_SIZE];
+        uint8_t memory[MEM_SIZE];
+
+        // General Registers
         uint16_t AF = 0;
         uint16_t BC = 0;
         uint16_t DE = 0;
         uint16_t HL = 0;
+
+        // Hardware Registers
+        uint8_t LCDC = 0;                                      // LCD Control
+
         int PC;
         int SP;
         bool isRunning = false;
@@ -21,6 +29,9 @@ class Processor {
         Processor(uint8_t cartType);
         void tickClock();
         void bootSequence(Cartridge cart);
+        uint16_t fetch(int PC);
+        array<uint8_t, 2> decode(uint8_t opCode);
+        
 
 };
 
--- src/Processor.cpp
+++ src/Processor.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <chrono>
 #include <cstdint>
+#include <array>
 #include "../include/Processor.hpp"
 #include "../include/Cartridge.hpp"
 
 using namespace std;
-
-const uint16_t VRAM_START = 0x8000;
 
 Processor::Processor(uint8_t cartType) {
 
@@ -14,6 +13,14 @@ Processor::Processor(uint8_t cartType) {
 
 void Processor::tickClock() {
     // 238 nanoseconds
+    // Perform single instruction:
+        // Take start time
+        // Fetch Instruction w/ t-time & m-time
+        // Calculate time it should take to complete instruction
+        // Decode and Perform instruction
+        // Calculate time delta from instructions start to end
+        // Sleep for time delta from instruction time taken to calculated completion time.
+        
 }
 
 void Processor::bootSequence(Cartridge cart) {
@@ -25,5 +32,26 @@ void Processor::bootSequence(Cartridge cart) {
         memory[vramIndex] = cart.cartROM[i];
         vramIndex++; 
     }
+    
+}
 
+uint16_t Processor::fetch(int PC) {
+    
+    uint8_t opCode = memory[PC];
+    PC++;
+    return opCode;
+
+}
+
+array<uint8_t, 2> Processor::decode(uint8_t opCode) {
+
+    uint8_t highMask = 0xF0;
+    uint8_t lowMask = 0x0F;
+
+    uint8_t highNibble = (highMask && opCode) >> 4;
+    uint8_t lowNibble = (lowMask && opCode);
+
+    array<uint8_t, 2> nibbles = {highNibble, lowNibble};
+    
+    return nibbles;
 }
