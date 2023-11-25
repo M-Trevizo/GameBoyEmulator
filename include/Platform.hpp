1ff9d98e5a0d8058e019d14167f8dd7ebeba7117
Michael Trevizo <michael.trevizo93@yahoo.com>
2023-11-25 02:33:38 -0800

    Add constants to set up FPS limit

--- include/Platform.hpp
+++ include/Platform.hpp
@@ -7,15 +7,17 @@ class Platform {
 
     public:
 
-    static const int WIDTH = 800;
-    static const int HEIGHT = 720;
-    SDL_Renderer *renderer;
-    SDL_Window *window;
-    SDL_Event event;
+        static const int WIDTH = 800;
+        static const int HEIGHT = 720;
+        static const int FPS_LIMIT = 60;
+        static const int TICKS_PER_FRAME = 1000 / FPS_LIMIT;
+        SDL_Renderer *renderer;
+        SDL_Window *window;
+        SDL_Event event;
 
-    Platform();
-    ~Platform();
-    bool processEvents();
+        Platform();
+        ~Platform();
+        bool processEvents();
     
 };
 
