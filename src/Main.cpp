b8246e51895656d28f68b810d5947c3c67e578f8
Michael Trevizo <michael.trevizo93@gmail.com>
2024-01-26 14:33:43 -0800

    Increment cycle counter

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <array>
 #include "../include/Platform.hpp"
 #include "../include/Cartridge.hpp"
+#include "../include/Processor.hpp"
 
 using namespace std;
 
@@ -15,12 +17,23 @@ int main(int argc, char** argv) {
 
     Cartridge cartridge(path);
     Platform platform;
+    Processor processor(cartridge);
     
     bool quit = false;
     while(!quit) {
         
         quit = platform.processEvents();
         
+        // Emulation Loop
+        int cycleCount = 0;
+        while(cycleCount <= M_CYCLES_PER_FRAME) {
+            uint8_t opCode = processor.fetch();
+            array<uint8_t, 2> nibbles = processor.decode(opCode);
+            cycleCount += processor.execute(nibbles);
+            // TODO: make sure to execute correct # of cycles
+        }
+        
+
     }
 
     return 1;
