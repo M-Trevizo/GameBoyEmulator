ff2e09059b862a2411e51818375060ab9de9de80
Michael Trevizo <michael.trevizo93@gmail.com>
2024-02-03 17:26:40 -0800

    Writing more tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -22,15 +22,81 @@ TEST_CASE("Testing Fetch and that PC increments after each Fetch call") {
     CHECK(processor.fetch() == 0xC3);
 }
 
-TEST_CASE("Testing Load instructions") {
-    processor.PC = 0x0104;
-    REQUIRE(processor.PC == 0x0104);
+TEST_CASE("Testing Load Instructions") {
+    
 
     SUBCASE("Testing 16-Bit Loads") {
+        processor.PC = 0x0104;
+        processor.AF.high = 0x12;
+        REQUIRE(processor.PC == 0x0104);
+
         CHECK(processor.LD_16BIT(processor.BC.word) == 3);
         CHECK(processor.BC.word == 0xCEED);
         CHECK(processor.BC.high == 0xCE);
         CHECK(processor.BC.low == 0xED);
+
+        CHECK(processor.LD_16BIT_A(processor.BC.word) == 2);
+        CHECK(processor.memory[processor.BC.word] == 0x12);
     }
 
+    SUBCASE("Testing 8-Bit Loads") {
+        
+        processor.PC = 0x0104;
+        CHECK(processor.LD_8BIT_H(processor.BC.high) == 2);
+        CHECK(processor.BC.high == 0xCE);
+
+        processor.PC = 0x0104;
+        CHECK(processor.LD_8BIT_L(processor.BC.low) == 2);
+        CHECK(processor.BC.low == 0xCE);
+
+        processor.BC.word = 0x0012;
+        CHECK(processor.LD_A_16BIT(processor.BC.word) == 2);
+        CHECK(processor.memory[processor.BC.word] == 0xFF);
+        
+        processor.DE.word = 0x0064;
+        processor.LD_A_16BIT(processor.DE.word);
+        CHECK(processor.memory[processor.DE.word] == 0xE0);
+    }
+
+}
+
+TEST_CASE("Testing Arithmetic Instructions") {
+    processor.BC.word = 0x11AA;
+
+    SUBCASE("Testing 16-bit Incrememnt and Decrement") {
+        CHECK(processor.INC_16BIT(processor.BC.word) == 2);
+        CHECK(processor.BC.word == 0x11AA + 1);
+        CHECK(processor.DEC_16BIT(processor.BC.word) == 2);
+        CHECK(processor.BC.word == 0x11AA);
+
+    }
+
+    processor.BC.word = 0x0001;
+    processor.HL.word = 0x0002;
+    processor.AF.low |= N_FLAG;
+    REQUIRE((processor.AF.low & N_FLAG) == N_FLAG);
+    SUBCASE("Testing 16-Bit Add") {
+        CHECK(processor.ADD_HL_R16(processor.BC.word) == 2);
+        CHECK(processor.HL.word == 0x0003);
+        
+        SUBCASE("Testing that N-Flag gets unset") {
+            CHECK((processor.AF.low & N_FLAG) == 0);
+        }
+
+        processor.BC.word = 0x0FFF;
+        processor.HL.word = 0x0001;
+        SUBCASE("Testing H-Flag") {
+            processor.ADD_HL_R16(processor.BC.word);
+            uint8_t flag = processor.AF.low & H_FLAG;
+            CHECK(flag == H_FLAG);
+        }
+
+        processor.BC.word = 0xFFFF;
+        processor.HL.word = 0x0001;
+        SUBCASE("Testing C-Flag") {
+            processor.ADD_HL_R16(processor.BC.word);
+            uint8_t flag = processor.AF.low & C_FLAG;
+            CHECK(flag == C_FLAG);
+        }
+    }
 }
